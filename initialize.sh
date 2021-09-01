07d9a9fc815b3ba9ae52267d3bd18616bcaf7e8a
Stuart R. Kirk <Stuart.Kirk@microsoft.com>
2021-08-31 21:11:30 -0400

    Ansiblefest Updates

--- initialize.sh
+++ initialize.sh
@@ -5,29 +5,29 @@
 # Written by Stuart Kirk <stuart.kirk@microsoft.com>
 
 # Init
-if [ -f "vars-myvars.yml" ]; then
-    echo "This script has already been run and your vars-myvars.yml file already exists."
+if [ -f "vars-myvars.yaml" ]; then
+    echo "This script has already been run and your vars-myvars.yaml file already exists."
     exit 1
 fi
 
 # Variables
 RANDNUM="$(shuf -i 20000-50000 -n 1)"
-AZUREDC="$(az group list -o table |grep ansiblefest2021 | awk '{print $2}')"
 RG="ansiblefest2021"
-USERNANE="$(whoami)"
+USERNAME="$(whoami)"
 VMNAME="$(echo $USERNAME | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//g' | cut -c 1-15)"
+AZUREDC="$(cat ../az.region)"
 
 # Operations
-cp vars.yml vars-myvars.yml
-sed -i "s/MYRANDOM/$RANDNUM/g" vars-myvars.yml
-sed -i "s/MY_RESOURCE_GROUP/$RG/g" vars-myvars.yml
-sed -i "s/MYVM/$VMNAME/g" vars-myvars.yml
-sed -i "s/MYAZUREDATACENTER/$AZUREDC/g" vars-myvars.yml
-sed -i "s/MYUSERNAME/$USERNAME/g" vars-myvars.yml
+cp vars.yaml vars-myvars.yaml
+sed -i "s/MYRANDOM/$RANDNUM/g" vars-myvars.yaml
+sed -i "s/MY_RESOURCE_GROUP/$RG/g" vars-myvars.yaml
+sed -i "s/MYVM/$VMNAME-vm/g" vars-myvars.yaml
+sed -i "s/MYAZUREDATACENTER/$AZUREDC/g" vars-myvars.yaml
+sed -i "s/MYUSERNAME/$USERNAME/g" vars-myvars.yaml
 
 # Finish
 echo " "
-echo "Your custom variables file, vars-myvars.yml, has been created.  Go forth and conquer!"
+echo "Your custom variables file, vars-myvars.yaml, has been created.  Go forth and conquer!"
 exit 0
 
 
